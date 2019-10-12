171ce1616fd36f74f6835742cabea4966ba97e31
wzym@yandex.ru <Afdqqq*___92>
2019-10-12 15:47:01 +0300

    Works.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -9,11 +10,32 @@ public class Main {
 
     private void run() throws IOException {
         BufferedReader bf = new BufferedReader(new FileReader("Test.txt"));
-        int number = Integer.parseInt(bf.readLine());
-        System.out.println(countAmount(number));
+        int inNumber = Integer.parseInt(bf.readLine());
+        System.out.println(countAmount(inNumber));
     }
 
-    private int countAmount(int number) {
-        return 0;
+    private int countAmount(int numToCountStepsBefore) {
+        int[] dynamics = getArrayForDyn(numToCountStepsBefore);
+        for (int i = 1; i < numToCountStepsBefore; i++) {
+            int currentStepAmount = dynamics[i] + 1;
+            for (int val :
+                    getNewValues(i)) {
+                if ((val <= numToCountStepsBefore) && (dynamics[val] > currentStepAmount)) {
+                    dynamics[val] = currentStepAmount;
+                }
+            }
+        }
+        return dynamics[numToCountStepsBefore];
+    }
+
+    private int[] getArrayForDyn(int lastElementIndex) {
+        int[] result = new int[lastElementIndex + 1];
+        Arrays.fill(result, Integer.MAX_VALUE);
+        result[1] = 0;
+        return result;
+    }
+
+    private int[] getNewValues(int curNum) {
+        return new int[] { curNum + 1, curNum * 2, curNum * 3 };
     }
 }
