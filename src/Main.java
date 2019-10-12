2cf4805cc19887f2f5301a1d6a56aa5550bd2737
wzym@yandex.ru <Afdqqq*___92>
2019-10-12 16:32:17 +0300

    It works. Quite fast.

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -11,7 +9,7 @@ public class Main {
     }
 
     private void run() throws IOException {
-        BufferedReader bf = new BufferedReader(new FileReader("Test.txt"));
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         int inNumber = Integer.parseInt(bf.readLine());
         print(countAmount(inNumber));
     }
